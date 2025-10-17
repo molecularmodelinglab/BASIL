4d493402637d96ae7678e1c216372846991f8b6d
kelvin paschal <kelvinidanwekhai@gmail.com>
2025-10-17 15:29:00 -0400

    LinearKernel doesn't accept some inputs

--- app/bayesopt/kernels/k1.py
+++ app/bayesopt/kernels/k1.py
@@ -20,11 +20,11 @@ class DotProductKernel(Kernel):
     def to_gpytorch(self, ard_num_dims=None, batch_shape=torch.Size([]), active_dims=None):
         # Map your custom kernel to an existing gpytorch kernel
         return gpytorch.kernels.LinearKernel(
-            num_dims=ard_num_dims,
+            # num_dims=ard_num_dims,
             batch_shape=batch_shape,
             active_dims=active_dims,
-            variance_prior=None,
-            bias_prior=None,
+            # variance_prior=None,
+            # bias_prior=None,
         )
 
 
