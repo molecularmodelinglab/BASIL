f1d7d384709d79170adb3f2003c2b98ab1e4fe04
idanwekhai <kelvinidanwekhai@gmail.com>
2026-08-20 14:20:31 -0400

    updated kernel defiition to follow new baybe spec

--- app/bayesopt/kernels/k1.py
+++ app/bayesopt/kernels/k1.py
@@ -1,36 +1,18 @@
 import gpytorch
 import torch
-from baybe.kernels import AdditiveKernel, MaternKernel, RQKernel
+from baybe.kernels import AdditiveKernel, MaternKernel, RQKernel, LinearKernel
 from baybe.kernels.base import Kernel
 from baybe.surrogates.gaussian_process.components.kernel import PlainKernelFactory
 
 
-class DotProductKernel(Kernel):
-    def __init__(self, sigma=1.0):
-        super().__init__()
-        object.__setattr__(self, "_sigma", sigma)
-
-    @property
-    def sigma(self):
-        return self._sigma
-
-    def forward(self, X, Y):
-        return self.sigma**2 + torch.mm(X, Y.T)
-
-    def to_gpytorch(self, ard_num_dims=None, batch_shape=torch.Size([]), active_dims=None):
-        return gpytorch.kernels.LinearKernel(
-            batch_shape=batch_shape,
-            active_dims=active_dims,
-        )
-
 
 def gp_kernel():
-    dot_product_kernel = DotProductKernel(sigma=0.01)
+    linear_kernel = LinearKernel(variance_initial_value=0.01)
 
     rq_kernel = RQKernel(lengthscale_initial_value=0.01)
     matern_kernel = MaternKernel(lengthscale_initial_value=0.1, nu=1.5)
 
-    kernel = AdditiveKernel((dot_product_kernel, rq_kernel, matern_kernel))
-    kernel_factory = PlainKernelFactory(kernel=kernel)
+    kernel = AdditiveKernel((linear_kernel, rq_kernel, matern_kernel))
+    kernel_factory = PlainKernelFactory(component=kernel)
 
     return kernel_factory
